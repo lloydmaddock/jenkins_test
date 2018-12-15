ecda986042684e6ede0b19fff7cfbaa7f3a55133
lloydmaddock <lloyd.maddock@gmail.com>
2018-12-15 13:58:25 +0000

    Comment out even more junit test

--- src/main_package/TestAddSomeNumbers.java
+++ src/main_package/TestAddSomeNumbers.java
@@ -1,8 +1,8 @@
 package main_package;
 
-import static org.junit.jupiter.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 
 class TestAddSomeNumbers {
 
