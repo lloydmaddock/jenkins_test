776be581e6f9d54b166286427afa8d4d3973f9c1
lloydmaddock <lloyd.maddock@gmail.com>
2018-12-15 20:12:22 +0000

    Deliberately fail unit test

--- src/test/java/jenkins_unit_tests.java
+++ src/test/java/jenkins_unit_tests.java
@@ -12,7 +12,8 @@ class jenkins_unit_tests {
 	void test() {
 		float result = main.java.jenkins_test_main.addSomeNumbers(1.0f,2.0f,3.0f,4.0f);
 		
-		assert(result==10.0f);
+		//assert(result==10.0f);
+		assert(result==11.0f);
 	}
 
 }
