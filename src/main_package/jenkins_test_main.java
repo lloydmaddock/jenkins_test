09c4d3813fb8e36b4c4eef48d688d21a30e8e956
lloydmaddock <lloyd.maddock@gmail.com>
2018-12-15 12:51:47 +0000

    First Commit

---
+++ src/main_package/jenkins_test_main.java
@@ -0,0 +1,25 @@
+package main_package;
+
+public class jenkins_test_main {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Jenkins");
+		
+		float result = addSomeNumbers(1.0f,2.0f,3.0f);
+		
+		System.out.println("Result is "+result);
+	}
+	
+	public static float addSomeNumbers(float... numbers)
+	{
+		float total=0.0f;
+		
+		for(float number:numbers)
+		{
+			total+=number;
+		}
+		return total;
+	}
+
+}
