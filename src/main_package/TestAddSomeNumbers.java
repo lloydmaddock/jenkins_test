dd7befea324004007b55ef67582f6db453758054
lloydmaddock <lloyd.maddock@gmail.com>
2018-12-15 13:56:41 +0000

    Comment out junit test.

--- src/main_package/TestAddSomeNumbers.java
+++ src/main_package/TestAddSomeNumbers.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.Test;
 
 class TestAddSomeNumbers {
 
+	/*
 	@Test
 	void test() {
 		float result = jenkins_test_main.addSomeNumbers(1.0f,2.0f,3.0f,4.0f);
 		
 		assert(result==10.0f);
 	}
-
+	 */
 }
