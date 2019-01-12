227a957b6322e726188f61a3ef1f6be318a5c370
lloydmaddock <lloyd.maddock@gmail.com>
2019-01-12 18:01:39 +0000

    kasa test on

--- src/test/java/Test_jenkins_unit.java
+++ src/test/java/Test_jenkins_unit.java
@@ -11,7 +11,7 @@ class Test_jenkins_unit {
 	@Test
 	void test() {
 		float result = main.java.jenkins_test_main.addSomeNumbers(1.0f,2.0f,3.0f,4.0f);
-		xxxx
+		
 		assert(result==10.0f);
 		//assert(result==11.0f);
 	}
